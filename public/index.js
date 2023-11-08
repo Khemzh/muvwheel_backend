f1d1a2a9fbd3b6688f4cddf0d27c2ebbd342217f
AltoTechSharedAccount <nutnai3302@gmail.com>
2023-11-09 01:17:33 +0700

    fix from d169f0d by NX_Branch🐛

--- public/index.js
+++ public/index.js
@@ -1,9 +1,14 @@
+<<<<<<< Updated upstream
 const test = require("./test")
+=======
+// const map_api = require("./map_api")
+>>>>>>> Stashed changes
 
-const express = require('express');
-const app = express();
-const port = 3000;
+// const express = require('express');
+// const app = express();
+// const port = 3000;
 
+<<<<<<< Updated upstream
 app.get('/', (req, res) => {
     var x = test.testa()
     res.send(x);
@@ -13,3 +18,51 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
 });
+=======
+// var num = 2;
+
+// if (num == 1) {
+//     app.get('/', (req, res) => {
+//         var x = map_api.renderMap()
+//         res.send(x);
+//     });
+// } else if (num == 2) {
+//     app.get('/', (req, res) => {
+//         var y = map_api.initMaps()
+//         res.send(y);
+//     });
+// }
+
+// /* app.get('/', (req, res) => {
+//     var x = map_api.renderMap()
+//     res.send(x);
+// }); */
+
+// /* app.get('/', (req, res) => {
+//     var y = map_api.initMaps()
+//     res.send(y);
+// }); */
+
+// /* app.get('/', (req, res) => {
+//     var z = map_api.renderMapLeafLet()
+//     res.send(z);
+// }); */
+
+// app.listen(port, () => {
+//     console.log(`Listening at http://localhost:${port}`);
+// });
+
+
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();
+>>>>>>> Stashed changes
---
+++ public/map_api.js
@@ -0,0 +1,71 @@
+// const { Loader } = require("@googlemaps/js-api-loader");
+
+//import { Loader } from "@googlemaps/js-api-loader"
+//Loader = require("@googlemaps/js-api-loader")
+let map;
+
+async function initMap() {
+
+    // const loader = new Loader({
+    //     apiKey: "AIzaSyCUP4lwuTEXSPnFmJIY_eGSEnOGDGPxMRg",
+    //     version: "weekly"
+    // });
+
+    // loader.load().then(async () => {
+    //     const { Map } = await google.maps.importLibrary("maps");
+
+    //     map = new Map(document.getElementById("map"), {
+    //       center: { lat: 13.7563, lng: 100.5018 },
+    //       zoom: 8,
+    //     });
+    // }).catch((err) => {
+    //     console.error(err);
+    // });
+    const { Map } = await google.maps.importLibrary("maps");
+
+    map = new Map(document.getElementById("map"), {
+        center: { lat: 13.7563, lng: 100.5018 },
+        zoom: 17,
+    });
+}
+
+export function initMaps() {
+    initMap();
+}
+
+function renderMap() {
+    // Create a simple HTML page with a map container
+    const html = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Google Maps Example</title>
+        <script
+          src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCUP4lwuTEXSPnFmJIY_eGSEnOGDGPxMRg&callback=initMap"
+          async
+          defer
+        ></script>
+        <script
+         async
+         defer>
+          function initMap() {
+            // Create a map centered at a specific location
+            var map = new google.maps.Map(document.getElementById("map"), {
+              center: { lat: 13.7563, lng: 100.5018 },
+              zoom: 8,
+            });
+          }
+        </script>
+      </head>
+      <body>
+        <div id="map" style="height: 775px; width: 100%;"></div>
+      </body>
+    </html>`;
+
+    return html;
+}
+initMaps();
+
+//module.exports = {renderMap}
+// module.exports = {initMaps}
+window.initMaps = initMaps;
