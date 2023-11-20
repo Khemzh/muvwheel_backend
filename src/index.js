168297701874b906388d6262f856414b0d0c5002
AltoTechSharedAccount <nutnai3302@gmail.com>
2023-11-21 02:10:41 +0700

    add dotenv✨

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const { database, auth } = require('./firebase.config')
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
+require('dotenv').config() // Load environment variables from .env file
+const port = process.env.PORT
 
 const app = express()
 
@@ -10,69 +12,73 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
 app.get('/', async (req, res) => {
-  const snapshot = await database.get()
-  const list = snapshot.docs.map((doc) => ({ ...doc.data() }))
-  res.send(list)
+    res.send('Ready to call API!')
+})
+
+app.get('/getDB', async (req, res) => {
+    const snapshot = await database.get()
+    const list = snapshot.docs.map((doc) => ({ ...doc.data() }))
+    res.send(list)
 })
 
 app.post('/getuid', (req, res) => {
-  uid = req.body.uid ?? ''
-  firebaseToken = req.body.firebaseToken ?? ''
+    uid = req.body.uid ?? ''
+    firebaseToken = req.body.firebaseToken ?? ''
 
-  if (!uid || !firebaseToken) {
-    return res.status(400).send({
-      msg: 'Bad request',
-    })
-  }
-
-  auth
-    .verifyIdToken(firebaseToken)
-    .then((decodedToken) => {
-      const decodedUid = decodedToken.uid
-      if (uid != decodedUid) {
-        return res.status(401).send({
-          msg: 'token ผิด',
+    if (!uid || !firebaseToken) {
+        return res.status(400).send({
+            msg: 'Bad request',
         })
-      }
-    })
-    .catch((error) => {
-      return res.status(401).send({
-        msg: 'token ผิด',
-      })
-    })
+    }
 
-  database
-    .where('uid', '==', uid)
-    .get()
-    .then(function (querySnapshot) {
-      if (querySnapshot.empty) {
-        return res.status(404).send({
-          msg: 'Not found',
+    auth
+        .verifyIdToken(firebaseToken)
+        .then((decodedToken) => {
+            const decodedUid = decodedToken.uid
+            if (uid != decodedUid) {
+                return res.status(401).send({
+                    msg: 'token ผิด',
+                })
+            }
         })
-      } else {
-        querySnapshot.forEach(function (doc) {
-          const token = jwt.sign(doc.data(), 'catzero1337')
+        .catch((error) => {
+            return res.status(401).send({
+                msg: 'token ผิด',
+            })
+        })
 
-          return res.status(200).send({
-            token: token,
-          })
+    database
+        .where('uid', '==', uid)
+        .get()
+        .then(function (querySnapshot) {
+            if (querySnapshot.empty) {
+                return res.status(404).send({
+                    msg: 'Not found',
+                })
+            } else {
+                querySnapshot.forEach(function (doc) {
+                    const token = jwt.sign(doc.data(), 'catzero1337')
+
+                    return res.status(200).send({
+                        token: token,
+                    })
+                })
+            }
         })
-      }
-    })
-    .catch(function (error) {
-      console.log('Error getting documents: ', error)
-      return res.status(400).send({
-        msg: 'Bad request',
-      })
-    })
+        .catch(function (error) {
+            console.log('Error getting documents: ', error)
+            return res.status(400).send({
+                msg: 'Bad request',
+            })
+        })
 })
 
 app.post('/register', async (req, res) => {
-  const data = req.body
-  console.log(data)
-  await database.add(data)
-  const token = jwt.sign(data, 'catzero1337')
-  res.send({ token: token })
+    const data = req.body
+    console.log(data)
+    await database.add(data)
+    const token = jwt.sign(data, 'catzero1337')
+    res.send({ token: token })
 })
 
-app.listen(3001, () => console.log('💐 Server is running at 3001'))
+app.listen(port, () => console.log(`💐 Server is running at ${port}`))
